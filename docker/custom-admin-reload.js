ecfbfbe5f1c7772b2c89680ac49364f70bb7c999
Dhayalan <dhaya3486@gmail.com>
2026-08-10 11:24:44 +0530

    Delete docker directory

--- docker/custom-admin-reload.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('click', function(event) {
-    // Check if the clicked element is Flarum's admin "Save Changes" button
-    if (event.target && event.target.matches('button.Button--primary') && event.target.textContent.includes('Save Changes')) {
-        // Wait 1 second for the background API request to finish transmitting, then reload
-        setTimeout(function() {
-            location.reload();
-        }, 1000);
-    }
-});
